0f76fcfaaf897ae7d697886df0dd2ddf28d56daf
idriss atik <hadraskat@gmail.com>
2022-04-10 14:55:14 +0200

    sunnyside-agency Done

---
+++ sunnyside-agency-landing-page-main/app.js
@@ -0,0 +1,10 @@
+/* show and hide the menu fanction */
+
+function myFunction() {
+  let x = document.getElementById('click');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
+  }
+}
