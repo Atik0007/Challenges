f7bc30d36c012f89976b45f9d352dcaa4fb48752
idriss atik <hadraskat@gmail.com>
2022-04-14 18:04:12 +0200

    1.0.0 pricing-component

---
+++ pricing-component-with-toggle-master/app.js
@@ -0,0 +1,11 @@
+`use strict`;
+function annullyFunction() {
+  document.querySelector('#basic').innerHTML = '199.99';
+  document.querySelector('#profecional').innerHTML = '249.99';
+  document.querySelector('#master').innerHTML = '399.99';
+}
+function monthyFunction() {
+  document.querySelector('#basic').innerHTML = '19.99';
+  document.querySelector('#profecional').innerHTML = '24.99';
+  document.querySelector('#master').innerHTML = '39.99';
+}
